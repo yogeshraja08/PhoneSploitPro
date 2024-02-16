82f4683eb82e155b817b7a65b04ab693ce6aafca
YOGESH <yogeshraja45@gmail.com>
2024-02-16 17:33:18 +0530

    Update phonesploitpro.py

--- phonesploitpro.py
+++ phonesploitpro.py
@@ -1,29 +1,3 @@
-"""
-    COPYRIGHT DISCLAIMER
-
-    Script : PhoneSploit Pro - All in One Android Hacking ADB Toolkit  
-
-    Copyright (C) 2023  Mohd Azeem (github.com/AzeemIdrisi)
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-    Forking and modifying are allowed, but credit must be given to the
-    original developer, [Mohd Azeem (github.com/AzeemIdrisi)], and copying the code
-    is not permitted without permission.
-
-    For any queries, Contact me at : azeemidrisi@protonmail.com
-"""
 
 import os
 import random
@@ -35,7 +9,6 @@ import datetime
 import nmap
 from modules import banner
 from modules import color
-
 
 def start():
     # Creating Downloaded-Files folder if it does not exist
